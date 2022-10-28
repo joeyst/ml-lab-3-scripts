0cbe03e954680d03b8dc21d1813865d0ad06ae74
joeyst <joeystenbeck@gmail.com>
2022-10-27 18:52:43 -0700

    Try fixing train and test function for keras.

--- model_api/test_sequential.py
+++ model_api/test_sequential.py
@@ -7,17 +7,20 @@ def train_and_test_CV_tf(model, X, y, k=10):
   scores = []
   y_pred = []
 
-  for train, test in kfold:
-    X_train_tf, X_test_tf = ctt(X.values[train]), ctt(X.values[test])
-    y_train_tf, y_test_tf = ctt(y.values[train]), ctt(y.values[test])
-    y_test = y.values[test]
+  X = np.asarray(X.values).astype('float32')
+  y = np.asarray(y.values).astype('float32')
 
-    model.fit(X_train, y_train)
-    curr_y_pred = model.predict(X_test)
+  for train, test in kfold:
+    X_train_tf, X_test_tf = ctt(X[train]), ctt(X[test])
+    y_train_tf, y_test_tf = ctt(y[train]), ctt(y[test])
+    y_test = y[test]
+
+    model.fit(X_train_tf, y_train_tf)
+    curr_y_pred = model.predict(X_test_tf)
 
     y_pred.extend(curr_y_pred)
 
-    score = pearsonr(y_test, curr_y_pred)
+    score = pearsonr(y_test_tf, curr_y_pred)
     scores.append(score[0])
   
   return y_pred, np.mean(scores)
