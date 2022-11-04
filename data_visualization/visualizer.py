4414c694393405fb082692824c959a776f2517a2
joeyst <joeystenbeck@gmail.com>
2022-11-04 11:44:40 -0700

    Edit histogram.

--- data_visualization/visualizer.py
+++ data_visualization/visualizer.py
@@ -193,7 +193,7 @@ class DataVisualizer:
     fig, ax = plt.subplots(figsize=(20, 20))
 
     # plot data 
-    ax.hist(curr.values, kbins, density=True, histtype='step', stacked=False, fill=False, label=curr.columns)
+    ax.hist(curr.values, kbins, density=True, histtype='step', stacked=False, fill=True, label=curr.columns)
 
     plt.rc('xtick', labelsize=20)
     plt.rc('ytick', labelsize=20)
